278befe21ee040320db47af7357536471decf8c2
Rocio Martinez <rocio.martinez.civetta@gmail.com>
2021-04-08 10:41:53 -0300

    tira cualquier numero

--- src/prueba/main.java
+++ src/prueba/main.java
@@ -36,26 +36,30 @@ public static void main(String[] args) throws FileNotFoundException, IOException
 								if(game1.reel(1).cantCoincidencias(valorAct,l)>0) {
 									hitTot*=game1.reel(1).cantCoincidencias(valorAct,l);
 									jugada=4;
+									
 									for(int m = 0; m< game1.tamanioReel(4);m++) {
 										if(game1.reel(1).cantCoincidencias(valorAct,m)>0) {
 											hitTot*=game1.reel(1).cantCoincidencias(valorAct,m);
 											jugada=5;
-											game1.hit("Q").sumarHits(jugada, hitTot);//NO ME DEVUELVE EL HIT QUE QUIERO BUSCAR CON LA KEY QUE EL MANDO 
+											game1.hit(valorAct).sumarHits(jugada, hitTot);//NO ME DEVUELVE EL HIT QUE QUIERO BUSCAR CON LA KEY QUE EL MANDO 
 										}
+										game1.hit(valorAct).sumarHits(jugada, hitTot);
 									}
-									game1.hit(valorAct).sumarHits(jugada, hitTot);
+									
 								}
-							}
-							game1.hit(valorAct).sumarHits(jugada, hitTot);
+								game1.hit(valorAct).sumarHits(jugada, hitTot);
+							}						
 						}
+						game1.hit(valorAct).sumarHits(jugada, hitTot);
 					}
-					game1.hit(valorAct).sumarHits(jugada, hitTot);
+					
 				}
+				
 			}
 		}
 	}
-	game1.imprimirPremioHits();
 	
+		game1.imprimirPremioHits();
 }	
 }
 
